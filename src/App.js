97d707ffa751c848990b17dfc3431dbc3f580b40
kimminh061020 <kimminh061020@gmail.com>
2021-08-14 21:47:50 +0700

    audit and fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component }  from 'react';
-import React from 'react'
-import ReactDOM from 'react-dom'
 function App() {
   return (
     <div className="App">
